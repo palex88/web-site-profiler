82bee5ee72a8a64502f1a6134b8fb4bc045b9ff9
Alex Thompson <awt1088@gmail.com>
2020-11-18 10:20:55 -0800

    added proper docs

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,20 @@ extern crate clap;
 use clap::App;
 
 struct HttpReturn {
+    /// Struct to hold specific parts of the HTTP Response
     headers: Vec<String>,
     body: String,
     total_time: u128,
     response_size: usize,
 }
 
+/// Parse the cli args and sends out for processing.
+/// 
+/// CLI Args:
+/// 
+///     --url       -u: The full url to be tested
+///     --profile   -p: The number of times to run the test
+///     --help      -h: Show CLI help tool
 fn main() {
 
     let site: String;
@@ -24,11 +32,9 @@ fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     if let Some(url) = matches.value_of("url") {
-        // println!("Value for --url: {}", url);
         site = String::from(url);
         let (url, path) = parsr_url(site);
         if let Some(profile) = matches.value_of("profile") {
-            println!("Value for --profile: {}", profile);
             num_runs = String::from(profile).parse().unwrap();
         } else {
             num_runs = 1;
@@ -51,7 +57,16 @@ fn main() {
     }
 }
 
+/// Runs the program a given num of times
+/// Does not contain return, instead prints our summary of all runs
+/// 
+/// Args:
+/// 
+///     url: the root URL that should be tested
+///     path: additional URL path information
+///     num_runs: the number of times to run the tool
 fn run(url: String, path: String, num_runs: i32) {
+    
     let mut times: u128 = 0;
     let mut fastest: u128 = u128::MAX;
     let mut slowest: u128 = u128::MIN;
@@ -100,12 +115,22 @@ fn run(url: String, path: String, num_runs: i32) {
     println!("Smallest Response Size : {}", smallest);
     println!("Largest Response Size  : {}", largest);
     println!("Successfull Attempts   : {}/{}", response_codes["200"], median.len());
-    println!("Http Codes Present    :");
+    println!("Http Codes Present     :");
     for (code, num_times) in &response_codes {
         println!("\tCode: {}, Occurances: {}", code, num_times);
     }
 }
 
+/// Parses a URL into its root and path
+/// 
+/// Args:
+/// 
+///     site: the full URL
+/// 
+/// Ret:
+/// 
+///     url: the root URL
+///     path: url path information
 fn parsr_url(site: String) -> (String, String) {
 
     let mut parts : Vec<&str> = site.split("/").collect();
@@ -116,20 +141,28 @@ fn parsr_url(site: String) -> (String, String) {
     return (url, path)
 }
 
+/// Initiates a connection to a URL and sends a get request
+/// Times the run and returns basic info about each connection
+/// 
+/// Args:
+/// 
+///     url: the root URL
+///     path: url path information
+/// 
+/// Ret:
+/// 
+///     res: A result struct containing an HttpReturn struct
 fn connect(url: String, path: String) -> Result<HttpReturn> {
+
     let now = Instant::now();
     let url = format!("{}:80", url);
-    let mut stream = TcpStream::connect(url)?;
+    let mut stream = TcpStream::connect(url.clone())?;
 
-    // 2020-general-assignment.palex.workers.dev
     let mut request_data = String::new();
     request_data.push_str(&format!("GET /{} HTTP/1.0", path));
     request_data.push_str("\r\n");
-    request_data.push_str(&format!("Host: {}", "2020-general-assignment.palex.workers.dev"));
-    // request_data.push_str("Host: 2020-general-assignment.palex.workers.dev");
+    request_data.push_str(&format!("Host: {}", url));
     request_data.push_str("\r\n");
-    // request_data.push_str("Path: /links/");
-    // request_data.push_str("\r\n");
     request_data.push_str("Connection: close");
     request_data.push_str("\r\n");
     request_data.push_str("\r\n");
@@ -147,6 +180,16 @@ fn connect(url: String, path: String) -> Result<HttpReturn> {
     Ok(http_ret)
 }
 
+/// Splits up a returned Http response into the headers and the body
+/// 
+/// Args:
+/// 
+///     res: a vector or strings representing each line of the http response
+/// 
+/// Ret:
+/// 
+///     headers: vec containing each line of the headers
+///     body: string of the body of the response
 fn split_results(res: Vec<&str>) -> (Vec<String>, String) {
 
     let mut headers: Vec<String> = Vec::new();
